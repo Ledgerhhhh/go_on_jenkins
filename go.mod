223b142216d8b324e6101b1b777edc3a84ba0c99
ledgerhhhh <228664584@qq.com>
2023-11-19 16:33:58 +0800

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module ledger
+
+go 1.20
+
+replace github.com/apache/thrift => github.com/apache/thrift v0.13.0
+
+require (
+	github.com/apache/thrift v0.19.0 // indirect
+	github.com/cloudwego/hertz v0.7.2 // indirect
+)
---
+++ idl/hello.thrift
@@ -0,0 +1,30 @@
+namespace go hello.ledger
+
+struct HelloReq{
+    1: required string Id (api.query="id");
+    2: optional i32 age;
+    3: required list<string> hobbies
+}
+
+struct HelloResp{
+    1:string RespBody;
+}
+
+service HelloService{
+    HelloResp HelloMethod(1:HelloReq request)(api.get="/hello")
+}
+
+
+struct HelloReq2{
+    1: required string Id (api.query="id");
+    2: optional i32 age;
+    3: required list<string> hobbies
+}
+
+struct HelloResp2{
+    1:string RespBody;
+}
+
+service HelloService2{
+    HelloResp HelloMethod2(1:HelloReq request)(api.get="/hello2")
+}
